e1a796ecad674f6bdcbf1b72e13e8de986f95073
Jake Partusch <jakepartusch@gmail.com>
2019-02-18 21:58:32 -0600

    feat: functional lighthouse API deployed to AWS.

---
+++ README.md
@@ -0,0 +1,20 @@
+# Lighthouse as a Service (LaaS)
+
+LaaS is an API which interfaces with Chrome and [Lighthouse](https://github.com/GoogleChrome/lighthouse) to provide uditing, performance metrics, and best practices for Web Apps.
+
+### Example Request
+
+```
+https://lerm027s40.execute-api.us-east-1.amazonaws.com/dev/evaluate?url=https://google.com
+```
+
+### Example Response
+
+```
+{
+    "performance": 88,
+    "accessibility": 71,
+    "bestPractices": 93,
+    "seo": 80
+}
+```
---
+++ src/handler.js
@@ -0,0 +1,45 @@
+const lighthouse = require("lighthouse");
+const launchChrome = require("@serverless-chrome/lambda");
+
+const opts = {
+  chromeFlags: ["--headless"]
+};
+
+const launchChromeAndRunLighthouse = async (url, opts, config = null) => {
+  const chrome = await launchChrome({ flags: opts.chromeFlags });
+  opts.port = chrome.port;
+  const results = await lighthouse(url, opts, config);
+  await chrome.kill();
+  return results.lhr;
+};
+
+module.exports.evaluate = async event => {
+  console.log(JSON.stringify(event, null, 2));
+
+  const { queryStringParameters } = event;
+  const { url } = queryStringParameters;
+  if (!url) {
+    return {
+      statusCode: 400,
+      body: "url is required."
+    };
+  }
+
+  const results = await launchChromeAndRunLighthouse(url, opts);
+
+  const { categories } = results;
+  const { performance, accessibility, seo } = categories;
+  console.log(JSON.stringify(categories, null, 2));
+
+  const simpleScores = {
+    performance: performance.score * 100,
+    accessibility: accessibility.score * 100,
+    bestPractices: categories["best-practices"].score * 100,
+    seo: seo.score * 100
+  };
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(simpleScores)
+  };
+};
